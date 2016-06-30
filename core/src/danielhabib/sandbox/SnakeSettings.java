53c55cd1bcbe65b582933ae5773b77859ef8e549
danielhvs <danielhvs70@gmail.com>
2016-06-30 15:16:21 -0300

    Fixing level bug.

--- core/src/danielhabib/sandbox/SnakeSettings.java
+++ core/src/danielhabib/sandbox/SnakeSettings.java
@@ -3,19 +3,23 @@ package danielhabib.sandbox;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 
-
 public class SnakeSettings {
 	public static int level;
+
 	public static void load() {
 		Preferences prefs = Gdx.app.getPreferences("snakeSettings");
 		level = prefs.getInteger("level", 1);
 		prefs.putInteger("level", level);
 		prefs.flush();
 	}
+
 	public static void win() {
-		level++;
-		Preferences prefs = Gdx.app.getPreferences("snakeSettings");
-		prefs.putInteger("level", level);
-		prefs.flush();
+		// FIXME. do not hard-code.
+		if (level < 2) {
+			level++;
+			Preferences prefs = Gdx.app.getPreferences("snakeSettings");
+			prefs.putInteger("level", level);
+			prefs.flush();
+		}
 	}
 }
