729bb13e88667635add3a808fa95c66275e29432
danielhvs <danielhvs70@gmail.com>
2016-07-12 20:28:09 -0300

    Fixing button bug and beginning menu screen.

--- core/src/danielhabib/sandbox/MainMenu2.java
+++ core/src/danielhabib/sandbox/MainMenu2.java
@@ -22,9 +22,9 @@ public class MainMenu2 extends ScreenAdapter {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(36 / 225f, 20 / 225f, 116 / 225f, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		sl.getEngine().update(delta);
+		sl.getEngine().update(Gdx.graphics.getDeltaTime());
 	}
 
 }
